35ef26c64420dfffbb734b5e8defa2b2ec1a3a68
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-16 15:47:52 -0500

    Create Practice4.js

---
+++ WorkShop_JavaScript/Practice4.js
@@ -0,0 +1,10 @@
+var methodName = "Burrito";
+class Foo{
+  cosntructor(){}
+  ["Call"+"Me"](){  console.log("You called me!");
+  [methodName](){ console.log("Farris");}
+  }
+}
+var foo = new Foo();
+foo.CallMe();
+foo.Burrito();
