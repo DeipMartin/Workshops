5683caa76c40fa22cf88d42b06de97a885634d2f
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-16 15:42:22 -0500

    Update Practice3.js

--- WorkShop_JavaScript/Practice3.js
+++ WorkShop_JavaScript/Practice3.js
@@ -1,1 +1,16 @@
+class GetThings{
+  constructor(size){
+    this.length = size;
+  }
+  get Length(){
+    return this.length;
+  }
+  set Length(value){
+    this.length = value;
+    console.log("The value has been set ");
+  }
+}
+
+var thing = new GetThings(9);
+thing.Length
 
