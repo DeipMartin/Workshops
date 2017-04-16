1e3dfeace215e0a6cdfc96f1da7e31f8cce89b56
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-16 15:38:57 -0500

    Update Practice2.js

--- WorkShop_JavaScript/Practice2.js
+++ WorkShop_JavaScript/Practice2.js
@@ -18,7 +18,18 @@ class Person extends Human{
     return super.toString() + "I am a person and my age is " + this.age + " .";
   }
 }
-var  = new Person("Jose",40);
-class Child extends person {
-  
+
+var m = new Person("Jose",40);
+m.toString();
+
+class Child extends Person {
+  cosntructor(name,age){
+    super.constructor(name,age)
+  }
+  toString(){
+    return super.toString() + "I consider myself a achild";
+  }
 }
+
+var c = new Child("Guy",35);
+c.toString();
