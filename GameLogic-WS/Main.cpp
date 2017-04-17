ea84bc2b3d78f8ca20b4e1c7dad4ca763e4a1738
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-17 14:22:40 -0500

    Update Main.cpp

--- GameLogic-WS/Main.cpp
+++ GameLogic-WS/Main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
+
+#include "Character.h"
 using namespace std;
 
 int main(void){
+  Character c = Character("BOB");
+  cout<< c.GetName()<< " ; " << c.GetHealth();
+  cin.get();
+  
   return 0;
 }
