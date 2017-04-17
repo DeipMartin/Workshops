bbc71223f5df26da11106bf38d4325ad84edd7d3
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-04-17 14:20:09 -0500

    Update Character.cpp

--- GameLogic-WS/Character.cpp
+++ GameLogic-WS/Character.cpp
@@ -2,18 +2,26 @@
 Character::Character(){
   this->helth = 100;
 }
+CHaracter::Character(string n){
+   this->health= 100;
+   this->name = n;
+}
 
 Character::~Character(){
 }
+
 int Character::GetHealth(){
   return this->health;
 }
+
 void Character::Damage(int amount){
   this->health->amount;
 }
+
 string Character::GetName(){
   return this->name;
 }
+
 void Character::SetName(string n){
   this->name = n ;
 }
