034d73111eab75b2edbd6d3aeec46eb7bffd4dc5
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-05-02 00:20:17 -0500

    Create Practica1.php

---
+++ LearnPHP/Practica1.php
@@ -0,0 +1,15 @@
+/*Formulario*/
+<!DOCTYPE  html>
+<hmtl lang="es">
+  <head>
+    <title>Env&iacute;o de datos por GET y POST</title>
+    <meta charser="utf-8" />
+  </head>
+  <body>
+    <hgroup><h1>Formulario enviado por el m&eacute;todo GET</h1></hgroup>
+    <form>
+    
+    </form>
+  </body>
+  
+/*Datos */
