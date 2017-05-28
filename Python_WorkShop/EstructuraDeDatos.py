d15d1b7fd526585411fe65351702c12598a33f42
M. Leon Martin <DeipMartin@users.noreply.github.com>
2017-05-27 22:18:27 -0500

    Create EstructuraDeDatos.py

---
+++ Python_WorkShop/EstructuraDeDatos.py
@@ -0,0 +1,28 @@
+lista = [1,2,3,4,5]
+
+for i in range(len(lista):
+	print(lista[i])
+	print("index[%d] => %d"%(i,lista[i]))
+
+#ordenamiento burbuja
+
+lista = [4,2,6,8,5,7]
+for i in range(len(lista): #recorre la lista
+	for x in range(len(lista)-1): # recorre cada elemento
+		if lista[x] >lista[x+1] : # revisa que la condicion se cumpla
+			aux = lista[x]
+			lista[x] = lista [x+1]
+			lista [x+1] = aux
+			print (lista)
+
+#ordenamiento por seleccion
+lista = [4,2,6,8,5,7]
+for i in range(len(lista)):#recorre la lista
+	minimo = i
+	for x in range(i,len(lista)): # recorre cada elemento
+		if lista[x] < lista[minimo]: # revisa que la condicion se cumpla
+			minimo = x
+		aux = lista[i]
+		lista[i] = lista[minimo]
+		lista[minimo] = aux
+	print(lista)
